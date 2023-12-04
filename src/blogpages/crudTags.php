b5bfeb7fd07ac2ebece798839764a5bc20c08644
Maleknoura <maleknoura098@gmail.com>
2023-12-04 16:33:51 +0100

    added name to form

--- src/blogpages/crudTags.php
+++ src/blogpages/crudTags.php
@@ -2,19 +2,19 @@
 include "conn.php";
 session_start();
 //********************** ajouter tag **************************// 
-if(isset($_POST['submit'])){
-    $tagName=$_POST['tagname'];
+if (isset($_POST['submit'])) {
+    $tagName = $_POST['tagname'];
+    $insert = "INSERT INTO tags (tagName) values (?)";
+    $stmt = $conn->prepare($insert);
+    $stmt->bind_param("s", $tagName);
+    $stmt->execute();
+    $stmt->close();
 }
 
-$insert= "INSERT INTO tags (tagName) values (?)";
-$stmt = $conn->prepare($insert);
-$stmt->bind_param("s", $tagName);
-$stmt->execute();
-$stmt->close();
 
 //********* update tag *************//
-if(isset($_GET['tagId'])){
-    $tagId = $_GET['tagId'];  
+if (isset($_GET['tagId'])) {
+    $tagId = $_GET['tagId'];
     $tagName = $_POST['newname'];
     $update = "UPDATE tags SET tagName = ? WHERE tagId = ?";
     $stmt = $conn->prepare($updateQuery);
@@ -24,12 +24,11 @@ if(isset($_GET['tagId'])){
 }
 
 //*********delete tag ***********/
-if (isset($_GET['tagId'])){
-        $tagId = $_GET['tagid']; 
-        $delete = "DELETE FROM tags WHERE tagId = ?";
-        $stmt = $conn->prepare($delete);
-        $stmt->bind_param("i", $tagId);
-        $stmt->execute();
-        $stmt->close();
+if (isset($_GET['tagId'])) {
+    $tagId = $_GET['tagid'];
+    $delete = "DELETE FROM tags WHERE tagId = ?";
+    $stmt = $conn->prepare($delete);
+    $stmt->bind_param("i", $tagId);
+    $stmt->execute();
+    $stmt->close();
 }
-?>
